e8b2973c3830108037e2ee1a4138fb8085fc5088
VGurrasKpist <lyca23vu@student.ju.se>
2023-12-15 12:15:58 +0100

    Add project files.

---
+++ Inlämning 1 Kurslista/Models/Course.cs
@@ -0,0 +1,6 @@
+﻿namespace Inlämning_1_Kurslista.Models
+{
+    public class Course
+    {
+    }
+}
---
+++ Inlämning 1 Kurslista/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Inlämning_1_Kurslista.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
